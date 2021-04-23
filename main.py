6108e9b183eb2bf427fa981247567eb790cef348
bellrise <bellrise.av@gmail.com>
2021-04-23 20:45:05 +0200

    main.py: Add counter loop

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 # This is a simple counter
 
+import time
+
 __author__ = 'Listwas'
 
+# Wait for user input
+input('Wciśnij enter aby zacząć liczyć\n')
+
+counter = 1
+while True:
+    print(counter)
+    time.sleep(1)
+    counter += 1
