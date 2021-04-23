e7c5bc7c04a73dff9a322e7021bf5561b7184d3d
Listwas <patrickknives@gmail.com>
2021-04-23 21:50:42 +0200

    Better Counter

--- main.py
+++ main.py
@@ -7,11 +7,16 @@ __author__ = 'Listwas'
 # Wait for user input
 input('Press ENTER to start counting\n Hit CTRL+C to stop the counter')
 
-counter = 1
+sec = 1
+mins = 0
 try:
     while True:
-        print(counter, end='\r')
-        time.sleep(1)
-        counter += 1
+        print(f'    \r{mins:02}:{sec:02}', end='\r')
+        time.sleep(0.2)
+        sec += 1
+        if sec == 60:
+            sec = 0
+            mins += 1
+
 except KeyboardInterrupt:
-    print("\rCounting lasted: ", str(counter) + "s")
+    print("\rCounting lasted: ", str(mins) + "m", str(sec) + "s")
